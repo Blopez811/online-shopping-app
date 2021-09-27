ec64f6b33501db18f06d7def786dda0e277c4def
Benicio Lopez <benicio.lopez@gmail.com>
2021-09-26 22:42:29 -0500

    added foreign key to Product model

--- models/Product.js
+++ models/Product.js
@@ -25,6 +25,21 @@ Product.init(
       validate: {
         isDecimal: true
       }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Category',
+        key: 'id'
+      }
     }
   },
   {
