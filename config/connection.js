3736518bceba00992b749b65e4efc94a18b27953
Benicio Lopez <benicio.lopez@gmail.com>
2021-09-27 11:59:04 -0500

    added connection password hardcoded

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize(process.env.DB_NAME, 'root', 'Blopez@811',  {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
@@ -13,3 +13,5 @@ const sequelize = process.env.JAWSDB_URL
     });
 
 module.exports = sequelize;
+
+//process.env.DB_USER, process.env.DB_PW,  these are the second and third arguments for dynamic input on line 7
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,7 +5,23 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+    attributes: ['id', 'category_name'],
+    // be sure to include its associated Products
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+    .then(dbCategoryData => {
+      res.json(dbCategoryData)
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 router.get('/:id', (req, res) => {
