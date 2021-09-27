a0ff63081bcd5ff9d3f71c91ed49acc964e45206
Benicio Lopez <benicio.lopez@gmail.com>
2021-09-26 23:12:35 -0500

    added table relationships/associations in the index.js file

--- models/index.js
+++ models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'user_id'
+});
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  //as: 'not sure what to put here',
+  foreignKey: 'product_id'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  //as: 'not sure if this line is needed or what goes here',
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
