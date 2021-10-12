90296904858204d7e0893fe31c08f412d33e2745
Benicio Lopez <benicio.lopez@gmail.com>
2021-10-12 00:04:09 -0500

    added the get routes and delete route for products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -144,6 +144,23 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    individualHooks: true,
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbProductData => {
+    if (!dbProductData[0]) {
+        res.status(404).json({ message: 'No product found with this id' });
+        return;
+    }
+    res.json(dbProductData);
+})
+.catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+});
 });
 
 module.exports = router;
