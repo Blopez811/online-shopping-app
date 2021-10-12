37b9b0815a3cc7d4f86b77c0a310e1409f0d1255
Benicio Lopez <benicio.lopez@gmail.com>
2021-10-11 22:44:35 -0500

    got the Category get single category route by id running

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -27,6 +27,23 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'category_name']
+  })
+  .then(dbCategoryData => {
+    if (!dbCategoryData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+    }
+    res.json(dbCategoryData);
+})
+.catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+});
 });
 
 router.post('/', (req, res) => {
