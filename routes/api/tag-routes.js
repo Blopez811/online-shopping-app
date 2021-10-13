4c6afdc34998594270a7ae433bdcf935d5774a02
Benicio Lopez <benicio.lopez@gmail.com>
2021-10-13 09:50:03 -0500

    added put route for tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -68,6 +68,23 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    individualHooks: true,
+    where: {
+        id: req.params.id
+    }
+})
+    .then(dbUserData => {
+        if (!dbUserData[0]) {
+            res.status(404).json({ message: 'No tags found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
